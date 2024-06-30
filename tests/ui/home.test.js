71d7db18024ff4d2f53ae7da5dec5fba0c367b29
Nikola Nedanov <nedanovx@gmail.com>
2024-06-30 14:13:54 +0300

    fix UI tests

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -2,8 +2,8 @@ const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
     await page.goto('http://localhost:8080/');
-    const heading = await page.$('h2');
+    const heading = await page.$('p');
     const text = await heading.textContent();
-    expect(text).toContain('Home - Library');  
+    expect(text).toContain('Added books:');  
   });
   
